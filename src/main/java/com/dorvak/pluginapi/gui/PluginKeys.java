0147c5e9d8b2438aa2a95dc3cb16fb87f47a3d96
Matéo Papaïx (Dorvak) <mateo.papaix@gmail.com>
2024-06-21 20:10:48 +0200

    Adding FastBoard

--- src/main/java/com/dorvak/pluginapi/gui/PluginKeys.java
+++ src/main/java/com/dorvak/pluginapi/gui/PluginKeys.java
@@ -4,6 +4,6 @@ import org.bukkit.NamespacedKey;
 
 public class PluginKeys {
 
-    public static final NamespacedKey GUI_ACTION = new NamespacedKey("DORVAK-API-PLUGIN", "gui_action");
-    public static final NamespacedKey GUI_NAME = new NamespacedKey("DORVAK-API-PLUGIN", "gui_name");
+    public static final NamespacedKey GUI_ACTION = new NamespacedKey("dorvak-api-plugin", "gui_action");
+    public static final NamespacedKey GUI_NAME = new NamespacedKey("dorvak-api-plugin", "gui_name");
 }
